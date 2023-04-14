000c8f2b83eb15485527f542e7a1f5ee9117f595
Nabendu <nabendu.biswas@gmail.com>
2023-04-14 18:05:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const User = require('./models/user')
 const Task = require('./models/task')
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8080
 app.use(express.json())
 
 app.post('/users', (req, res) => {
